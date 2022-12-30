3aab960b840c25088469080147b97d48980eabd9
Alexey <lex1645@yandex.ru>
2022-12-30 22:55:48 +0300

    Создали массив строк

---
+++ Massiv/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] Mass()
+{
+    string[] Array = { "12", "37", "cat", "moscow", "123" };
+    Console.Write("[");
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{Array[i]}; ");
+    }
+    Console.Write("]");
+    return Array;
+}
+
+Mass();
+
